1b0f15f84e47e9272ac4c4364d6376cefcada180
mheannnatividad <maryannbernardo212@gmail.com>
2022-06-27 00:52:06 +0800

    add clear button code, and addition code for read button

--- contact-tracing-viewer/contact-tracing-viewer/Form1.vb
+++ contact-tracing-viewer/contact-tracing-viewer/Form1.vb
@@ -3,19 +3,28 @@
         Dim filenameReader As System.IO.StreamReader
         Dim filename As String = filenameBox.Text + ".txt"
 
-        filenameReader = My.Computer.FileSystem.OpenTextFileReader(filename)
+        If System.IO.File.Exists(filename) Then
+            filenameReader = My.Computer.FileSystem.OpenTextFileReader(filename)
+
+            While Not filenameReader.EndOfStream()
 
 
-        While Not filenameReader.EndOfStream()
+
+                If readTextBox.Text = "" Then
+                    readTextBox.Text = filenameReader.ReadLine()
+                End If
+
+                readTextBox.Text += vbNewLine + filenameReader.ReadLine()
+
+            End While
+        Else
+            MessageBox.Show("Invalid filename.")
+        End If
 
 
+    End Sub
 
-            If readTextBox.Text = "" Then
-                readTextBox.Text = filenameReader.ReadLine()
-            End If
-
-            readTextBox.Text += vbNewLine + filenameReader.ReadLine()
-
-        End While
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        readTextBox.Text = ""
     End Sub
 End Class
